359f6cc4816c4feba77bb5b6ded22b1786a6dd50
Oussema3 <ldrajmioussema2@gmail.com>
2020-12-09 17:32:55 +0000

    get mast digit of a random number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,14 +5,15 @@ f = "Last digit of"
 if number < 0:
     number = -number
     s = number % 10
+    number = -number
     s = -s
 
 else:
     s = number % 10
-    if s > 5:
-        print(f, number, "is", s, "and is greater then 5")
-    elif s == 0:
-        print(f, number, "is", s, "and is 0")
+if s > 5:
+    print(f, number, "is", s, "and is greater then 5")
+elif s == 0:
+    print(f, number, "is", s, "and is 0")
 
-    else:
-        print(f, number, "is", s, "and is less than 6 and not 0")
+else:
+    print(f, number, "is", s, "and is less than 6 and not 0")
