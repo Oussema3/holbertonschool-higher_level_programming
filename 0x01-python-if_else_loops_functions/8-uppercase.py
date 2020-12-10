be1d71305ceed914163e925313f9bc2f18167045
Oussema3 <ldrajmioussema2@gmail.com>
2020-12-10 16:46:00 +0000

    problems with emacs

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for i in str:
         if ord(i) > 96 and ord(i) < 123:
             Result = ord(i) - 32
-        print("{:c}".format(Result), end = "")
+        else:
             Result = ord(i)
-        print("{:c}".format(Result),end = "")
+        print("{:c}".format(Result), end="")
     print("")
