1ee04738c7289d59d313f2470f4d170c8940cc7f
Oussema3 <ldrajmioussema2@gmail.com>
2020-12-10 14:39:30 +0000

    return a string to upper case

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+def uppercase(str):
+    result = ""
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for i in str:
+        if i not in alphabet or alphabet.index(i) >= 26:
+            result += i
+        else:
+            result += alphabet[alphabet.index(i)+26]
+        
+    print(result)
