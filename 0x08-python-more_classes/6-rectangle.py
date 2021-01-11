278287d9894381e4d53fd254b5b9ae44d9c3b612
Oussema3 <ldrajmioussema2@gmail.com>
2021-01-11 23:05:46 +0000

    yeaah i confirm i was stupiddd

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -67,4 +67,4 @@ class Rectangle:
     def __del__(self):
         """ bitte mir sagen das ist nicht die ende HAHAHAHA """
         print("Bye rectangle...")
-        type(self).number_of_instances += 1
+        type(self).number_of_instances -= 1
