f9010a2afb679d94bfdc3e30ee16c7ae7c517990
Oussema3 <ldrajmioussema2@gmail.com>
2021-01-16 20:49:52 +0000

    best

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,6 +7,6 @@ def best_score(a_dictionary):
             if a_dictionary[i] > score:
                 score = a_dictionary[i]
                 defi = i
-        return name
-    elif a_dictionary is None or len(a_dictionary) == 0:
+        return defi
+    else:
         return None
