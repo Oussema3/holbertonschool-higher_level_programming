725f15638b6741f0289549be657043d99c7b1753
Oussema3 <ldrajmioussema2@gmail.com>
2021-01-21 01:28:11 +0000

    it's all about JSON

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,7 +7,8 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """
-     a function that writes an Object to a text file, using a JSON representation:
+     a function that writes an Object to a text file,
+    using a JSON representation:
     """
     with open(filename, mode='w', encoding="utf-8") as round:
         json.dump(my_obj, round)
