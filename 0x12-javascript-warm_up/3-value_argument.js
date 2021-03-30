3c13ae1d11eb2784c5060e2ab1aae045fbf0e80d
Oussema3 <ldrajmioussema2@gmail.com>
2021-03-30 02:36:23 +0000

    processArgv

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
+let x = 0;
+process.argv.forEach((val, index) => {
+  x++;
+  if (index === 2) {
+    console.log(`${val}`);
+  }
+});
+if (x <= 2) {
   console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-} 
+}
