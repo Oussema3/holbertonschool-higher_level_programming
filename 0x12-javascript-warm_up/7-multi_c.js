4262f40fbeac1c9c4740ddad755d4e6ae4a98bc7
garrech1 <garrech1@gmail.com>
2021-05-04 00:36:54 +0100

    whats wrong !

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const Script = 'C is fun';
-const i = Number(process.argv[2]);
+const x = Number(process.argv[2]);
 
-if (isNaN(i))  {
+if (isNaN(x))  {
     console.log('Missing number of occurrences');
 }
 else {
     let v=0;
-    while(v < i){
+    while(v < x){
 	console.log(Script);
 	v++;
     }
