d3506215ed548c90513196aebe120173b9a6adb2
garrech1 <garrech1@gmail.com>
2021-05-04 01:18:41 +0100

    semistandard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,8 +3,8 @@ const x = Number(process.argv[2]);
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-    let i;
-    for (i = 0; i < x; i++) {
-      console.log('C is fun');
-    }
+  let i;
+  for (i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
