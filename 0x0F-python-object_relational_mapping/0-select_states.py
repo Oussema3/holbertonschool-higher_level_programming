f4fff0c51be92f91746bc62ea25d9313e85fe507
Oussema3 <ldrajmioussema2@gmail.com>
2021-09-19 16:34:11 +0000

    DB

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 """
-Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+list all states
 """
-import sys
 import MySQLdb
+import sys
 
-
-def main(argv):
-    """connects to a given mysql database and lists all 'states' from it"""
-    conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=argv[1], passwd=argv[2], db=argv[3])
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+if __name__ == "__main__":
+    username = sys.argv[1]
+    pas = sys.argv[2]
+    dbn = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=pas, db=dbn)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
     cur.close()
-    conn.close()
-
-
-if __name__ == '__main__':
-    if len(sys.argv) == 4:
-        main(sys.argv)
+    db.close()
