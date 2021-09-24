2067303c939c7c699022a557b6ac4e4da22e0ec5
Oussema3 <ldrajmioussema2@gmail.com>
2021-09-24 09:01:35 +0000

    starwars

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
-const ar = [];
-const url = process.argv[2];
-request(url, function (err, response, body) {
-  if (err) { console.log(err); } else if (response.statusCode === 200) {
-    let count = 0;
-    for (const c of JSON.parse(body).results) {
-      ar.push(c.characters);
-    }
-    for (let i = 0, len = ar.length; i < len; i++) {
-      for (let j = 0, len2 = ar[i].length; j < len2; j++) {
-        if (ar[i][j] === 'https://swapi-api.hbtn.io/api/people/18/') { count++; }
+request.get(process.argv[2], function (error, response, body) {
+  if (error) console.log(error);
+  else if (response.statusCode === 200) {
+    let c = 0;
+    const results = JSON.parse(body).results;
+    const wedge = '/api/people/18/';
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].characters.find(el => el.includes(wedge))) {
+        c++;
       }
     }
-    console.log(count);
-  } else { console.log(''); }
+    console.log(c);
+  }
 });
